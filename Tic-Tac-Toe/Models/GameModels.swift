2a1534d814d05160a891884cb419e0ac1132d1e4
Lulu Wu <sky110423@gmail.com>
2024-06-19 15:15:14 -0700

    Game planning completed

--- Tic-Tac-Toe/Models/GameModels.swift
+++ Tic-Tac-Toe/Models/GameModels.swift
@@ -5,7 +5,7 @@
 //  Created by Lulu Wu on 6/19/24.
 //
 
-import Foundation
+import SwiftUI
 
 enum GameType {
     case single, bot, peer, undetermined
@@ -22,4 +22,45 @@ enum GameType {
             return ""
         }
     }
+}
+
+enum GamePiece: String {
+    case x, o
+    var image: Image {
+        Image(self.rawValue)
+    }
+}
+
+
+struct Player {
+    let gamePiece: GamePiece
+    var name: String
+    var moves: [Int] = []
+    var isCurrent = false
+    var isWinner: Bool {
+        for moves in Move.winningMoves {
+            if moves.allSatisfy(self.moves.contains) {
+                return true
+            }
+        }
+        return false
+    }
+}
+
+enum Move {
+    static var all = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+    static var winningMoves = [
+        // row
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+        // coloumn
+        [1, 4, 7],
+        [2, 5, 8],
+        [3, 6, 9],
+        // dialog
+        [1, 5, 9],
+        [3, 5, 7]
+    ]
 }
